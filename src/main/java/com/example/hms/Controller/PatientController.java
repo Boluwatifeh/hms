dfa1ae6241bbd5f6fe772682340a82cdc8a0131b
Boluwatifeh <tifeasypeasy@gmail.com>
2020-05-17 18:54:47 -0700

    added controller and service logic

---
+++ src/main/java/com/example/hms/Controller/PatientController.java
@@ -0,0 +1,48 @@
+package com.example.hms.Controller;
+
+import com.example.hms.Entity.Patient;
+import com.example.hms.Service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by T I F E on May, 2020
+ */
+@RestController
+@RequestMapping(value = "/api/v1")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/patients")
+    public @ResponseBody
+    List<Patient> getAllPatients(){
+        return patientService.getAllPatient();
+    }
+
+    @GetMapping("/patients/{id}")
+    public @ResponseBody
+    Patient getPatientById(@PathVariable int id){
+        return patientService.getPatientById(id);
+    }
+
+    @PostMapping("/patients")
+    public Patient addPatient(@RequestBody Patient patient){
+        return patientService.addPatient(patient);
+    }
+
+    @PutMapping("/update")
+    public Patient updatePatient(@RequestBody Patient patient){
+        return patientService.updatePatient(patient);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deletePatient(@PathVariable int id){
+       return patientService.deletePatient(id);
+    }
+
+
+}
---
+++ src/main/java/com/example/hms/Service/PatientService.java
@@ -0,0 +1,53 @@
+package com.example.hms.Service;
+
+import com.example.hms.Entity.Patient;
+import com.example.hms.Repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+/**
+ * Created by T I F E on May, 2020
+ */
+@Service
+public class PatientService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public List<Patient> getAllPatient(){
+        return (List<Patient>) patientRepository.findAll();
+    }
+
+    public Patient getPatientById(int id){
+        return patientRepository.findById(id).orElse(null);
+    }
+
+    public Patient addPatient(Patient patient){
+        return patientRepository.save(patient);
+    }
+
+    public String deletePatient(int id){
+        patientRepository.deleteById(id);
+        return "Patient deleted successfully!" + id;
+    }
+
+    public Patient updatePatient(Patient patient){
+        Patient existingPatient = patientRepository.findById(patient.getId()).orElse(null);
+        existingPatient.setFirst_name(patient.getFirst_name());
+        existingPatient.setMiddle_name(patient.getMiddle_name());
+        existingPatient.setDob(patient.getDob());
+        existingPatient.setAddress(patient.getAddress());
+        existingPatient.setGender(patient.getGender());
+        existingPatient.setPhone_number(patient.getPhone_number());
+        existingPatient.setBlood_group(patient.getBlood_group());
+        existingPatient.setGeno_type(patient.getGeno_type());
+        existingPatient.setRecord_status(patient.getRecord_status());
+        existingPatient.setDate_created(patient.getDate_created());
+        return patientRepository.save(existingPatient);
+
+    }
+
+}
