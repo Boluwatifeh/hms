27d053c09c18a8c4c04cdc926c9cef924e367153
Boluwatifeh <tifeasypeasy@gmail.com>
2020-05-17 18:39:47 -0700

    updated my fields

--- src/main/java/com/example/hms/Entity/Patient.java
+++ src/main/java/com/example/hms/Entity/Patient.java
@@ -1,12 +1,11 @@
 package com.example.hms.Entity;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Date;
 
 @Data
 @NoArgsConstructor
@@ -20,12 +19,13 @@ public class Patient {
     private String first_name;
     private String middle_name;
     private String last_name;
-    private int date_of_birth;
+    private Date dob;
     private String address;
     private String gender;
     private int phone_number;
     private String blood_group;
-    private String genotype;
+    private String geno_type;
     private String record_status;
-    private int date_created;
+    private String date_created;
+
 }
---
+++ src/main/java/com/example/hms/Repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.example.hms.Repository;
+
+import com.example.hms.Entity.Patient;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by T I F E on May, 2020
+ */
+public interface PatientRepository extends CrudRepository<Patient, Integer> {
+
+
+}
